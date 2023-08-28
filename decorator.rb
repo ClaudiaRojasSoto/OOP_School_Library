b89835ce1995aa5dba72efebc4b192ed1950ddfa
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-28 14:51:31 -0400

    Creates the decorator.rb file for the decorator class that inherits from nameable. On the constructor assign a nameable object from params to an instance variable, and implements the correct_name method that returns the result of the correct_name method of the nameable

---
+++ decorator.rb
@@ -0,0 +1,13 @@
+require_relative 'nameable'
+
+# This calss represent the decorator
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
