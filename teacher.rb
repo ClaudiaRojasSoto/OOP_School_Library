add1d7b73b6a99093b4cff7504b8b7bb7c7dcd5c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-28 13:40:52 -0400

    Add permission as a parameter giving it the value of true by default, and changes true instead trues to the method

--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,13 @@
 require_relative 'person'
 
-# Class representing a student
+# Class representing a teacher
 class Teacher < Person
-  def initialize(name, age, specialization)
-    super(name, age)
+  def initialize(name, age, specialization, permission: true)
+    super(name, age, permission)
     @specialization = specialization
   end
 
   def can_use_services?
-    trues
+    true
   end
 end
