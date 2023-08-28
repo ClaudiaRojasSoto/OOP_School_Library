d2af07d7ed697eaa808e297cb5ff15d7b4ab7b33
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-28 15:55:47 -0400

    Creates the book.rb file to the book class that initilized on the constructor the instance variables title and author, and add getters and setters instances to title, author and rentals

---
+++ book.rb
@@ -0,0 +1,11 @@
+# this class represents the books
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+end
