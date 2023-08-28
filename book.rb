af6a3557554af47281ea6f995d2e72091adc98ac
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-28 18:01:20 -0400

    Change the attr_accesor to include rentals

--- book.rb
+++ book.rb
@@ -1,7 +1,6 @@
 # this class represents the books
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+    attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
