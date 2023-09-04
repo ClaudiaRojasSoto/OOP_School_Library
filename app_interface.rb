cf2ad0724415bbb2d013c6457d433d3933674f07
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-04 14:22:02 -0500

    Create constructor method

--- app_interface.rb
+++ app_interface.rb
@@ -12,3 +12,27 @@ class AppInterface
   }.freeze
 
 end
+
+def initialize(app)
+    @app = app
+  end
+  def run
+    loop do
+      option = display_menu
+      handle_option(option)
+    end
+  end
+  def display_menu
+    puts "\nPlease choose an option:"
+    ACTIONS.each { |key, value| puts "#{key} - #{value.to_s.tr('_', ' ')}" }
+    print 'Option: '
+    gets.chomp.to_i
+  end
+  def handle_option(option)
+    action = ACTIONS[option]
+    if action
+      @app.send(action)
+    else
+      puts 'Invalid option. Please try again.'
+    end
+  end
