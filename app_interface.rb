69c9980a8d41df5f0a7a16259cc1fcb9c0b2cf99
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-04 16:14:14 -0300

    Create a new file called app_interface to add all the interface of the code to give main just to run the app. Add the inicitial code to run this file

---
+++ app_interface.rb
@@ -0,0 +1,14 @@
+require_relative 'app'
+
+class AppInterface
+  ACTIONS = {
+    1 => :list_books,
+    2 => :list_people,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rentals_for_person,
+    7 => :exit_app
+  }.freeze
+
+end
