e34f70c2376b71efc7548341451145bcf4764f2d
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-05 18:33:28 -0300

    Add to_h method designed to return an hash with all the relevants attributes of the instance rental

--- rental.rb
+++ rental.rb
@@ -3,11 +3,22 @@ class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
+    return unless book && person # Return if book or person is nil
+
     @date = date
     @book = book
     @person = person
 
     book.rentals << self
     person.rentals << self
+  end
+
+  # Método añadido para convertir el objeto a hash
+  def to_h
+    {
+      date: @date,
+      book_title: @book.title, # Usando el título del libro como un identificador único
+      person_id: @person.id # Suponiendo que las personas tienen un ID único
+    }
   end
 end
