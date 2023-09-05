5c73c865f77cdc245b980d04bda8e72c2ad268e2
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-05 19:24:07 -0300

    Fix linter errors and add data

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-require_relative 'json'
-require_relative 'fileutils'
+require 'json'
+require 'fileutils' # Línea agregada: para crear la carpeta
 require_relative 'book'
 require_relative 'person'
 require_relative 'student'
@@ -11,12 +11,12 @@ class App
   attr_accessor :books, :people, :rentals
 
   def initialize
-    FileUtils.mkdir_p('./data')
+    FileUtils.mkdir_p('./data') # Línea agregada: crea la carpeta 'data' si no existe
     @books = []
     @people = []
     @rentals = []
-    load_from_files
-  end
+    load_from_files # Línea agregada: carga datos al iniciar la app
+  end
 
   def list_books
     @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
@@ -121,10 +121,11 @@ class App
 
   def exit_app
     puts 'Exiting the application. Goodbye!'
-    save_to_files
+    save_to_files # agrega esta
     exit
   end
 
+  # Nuevas funciones agregadas para manejar JSON
   def save_to_files
     File.write('./data/books.json', JSON.dump(@books.map(&:to_h)))
     File.write('./data/people.json', JSON.dump(@people.map(&:to_h)))
@@ -134,20 +135,24 @@ class App
 
   def load_from_files
     @books = load_data('./data/books.json') { |book| Book.new(book['title'], book['author']) }
+
     @people = load_data('./data/people.json') do |person|
-     new_person = Person.new(person['name'], person['age'])
-     new_person.id = person['id']
-     new_person
+      new_person = Person.new(person['name'], person['age'])
+      new_person.id = person['id']
+      new_person
     end
+
     @rentals = load_data('./data/rentals.json') do |rental|
-     book = @books.find { |b| b.title == rental['book_title'] }
-     person = @people.find { |p| p.id == rental['person_id'] }
-     Rental.new(rental['date'], book, person) if book && person
+      book = @books.find { |b| b.title == rental['book_title'] }
+      person = @people.find { |p| p.id == rental['person_id'] }
+      Rental.new(rental['date'], book, person) if book && person
     end.compact
-   end
-   def load_data(file_name, &block)
+  end
+
+  def load_data(file_name, &block)
     return [] unless File.exist?(file_name)
+
     json_data = JSON.parse(File.read(file_name))
     json_data.map(&block)
-   end
+  end
 end
